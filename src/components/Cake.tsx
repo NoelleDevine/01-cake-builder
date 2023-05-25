99b37b22c9e84c68a72b1e99affaec4e04e766c7
chicletn <noelledevine@gmail.com>
2023-05-25 16:50:56 -0400

    end of Thursday

--- src/components/Cake.tsx
+++ src/components/Cake.tsx
@@ -1,7 +1,24 @@
+import Layer from "../models/Layer";
 import "./Cake.css";
+import CakeLayer from "./CakeLayer";
 
-const Cake = () => {
-  return <div className="Cake">Cake works</div>;
+interface Props {
+  propLayers: Layer[]; //child is receiving the array
+}
+
+const Cake = ({ propLayers }: Props) => {
+  return (
+    <div className="allLayers">
+      <ul className="fullCakeLayers">
+        {propLayers.map((item, index) => (
+          <CakeLayer
+            propLayer={item}
+            key={index + Math.random()} //this is specific to react, not the index
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Cake;
--- src/components/CakeLayer.tsx
+++ src/components/CakeLayer.tsx
@@ -1,7 +1,30 @@
+import Layer from "../models/Layer";
 import "./CakeLayer.css";
 
-const CakeLayer = () => {
-  return <div className="CakeLayer">CakeLayer works</div>;
+interface Props {
+  propLayer: Layer; //child is receiving the array
+}
+
+const CakeLayer = ({ propLayer }: Props) => {
+  console.log("testing 123");
+
+  return (
+    <div className="CakeLayer">
+      <p>
+        Testing:
+        {propLayer.height} {propLayer.width}
+      </p>
+
+      <div
+        style={{
+          width: propLayer.width + "px",
+          height: propLayer.height + "px",
+          backgroundColor: propLayer.color,
+          border: "2px solid black",
+        }}
+      ></div>
+    </div>
+  );
 };
 
 export default CakeLayer;
